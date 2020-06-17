fa09d4270cb1cb5a6c96d70e25117ee8d4a4e139
Bodnia, Konstantin <Konstantin.Bodnia@klm.com>
2020-06-17 13:20:20 +0200

    manager role to rule them all

--- src/context/FirebaseContext/index.js
+++ src/context/FirebaseContext/index.js
@@ -6,11 +6,13 @@ import "firebase/auth";
 export const FirebaseAppContext = React.createContext({
   firebase: {},
   initializing: true,
+  user: {},
 });
 
 const FirebaseAppContextProvider = ({ children, config }) => {
   const [firebaseApp, setFirebaseApp] = React.useState({});
   const [initializing, setInitializing] = React.useState(true);
+  const [user, setUser] = React.useState(null);
 
   React.useEffect(() => {
     const app = firebase.initializeApp(config);
@@ -25,12 +27,22 @@ const FirebaseAppContextProvider = ({ children, config }) => {
     hd: "frontmen.nl",
   });
 
+  firebase.auth().onAuthStateChanged(function (user) {
+    const docRef = firebase.firestore().collection("users").doc(user.uid);
+
+    docRef.get().then((userRec) => {
+      Object.assign(user, { userRec: userRec.data() });
+      setUser(user);
+    });
+  });
+
   return (
     <FirebaseAppContext.Provider
       value={{
         firebase: firebaseApp,
         initializing,
         provider: googleAuthProvider,
+        user,
       }}
     >
       {children}
