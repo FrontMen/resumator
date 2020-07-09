3274321a518fa425c181055e442bf261f6d65a94
Bodnia, Konstantin <Konstantin.Bodnia@klm.com>
2020-07-09 16:34:11 +0200

    underscore environmental variable

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ import LivePreviewer from "./pages/LivePreviewer";
 import Creator from "./pages/Creator";
 
 const {
-  REACT_APP_FIREBASE_API_KEY: apiKey,
-  REACT_APP_FIREBASE_AUTH_DOMAIN: authDomain,
-  REACT_APP_FIREBASE_DATABASE_URL: databaseURL,
-  REACT_APP_FIREBASE_PROJECT_ID: projectId,
-  REACT_APP_FIREBASE_STPRAGE_BUCKET: storageBucket,
-  REACT_APP_FIREBASE_MESSAGING_SENDER_ID: messagingSenderId,
-  REACT_APP_FIREBASE_APP_ID: appId,
+  _REACT_APP_FIREBASE_API_KEY: apiKey,
+  _REACT_APP_FIREBASE_AUTH_DOMAIN: authDomain,
+  _REACT_APP_FIREBASE_DATABASE_URL: databaseURL,
+  _REACT_APP_FIREBASE_PROJECT_ID: projectId,
+  _REACT_APP_FIREBASE_STPRAGE_BUCKET: storageBucket,
+  _REACT_APP_FIREBASE_MESSAGING_SENDER_ID: messagingSenderId,
+  _REACT_APP_FIREBASE_APP_ID: appId,
 } = process.env;
 
 const firebaseConfig = {
