ba947f4c81c81c560e53b1bec51e0745b716b9ec
Jan-Willem Baart <jwbaart@gmail.com>
2021-01-15 13:31:57 +0100

    fix: styling removed save button

--- src/components/LivePreviewerComponents/PreviewControls.tsx
+++ src/components/LivePreviewerComponents/PreviewControls.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react";
-import { Box, Button, makeStyles } from "@material-ui/core";
+import { Box, Button } from "@material-ui/core";
 import { DropdownButton, SpacedButton } from "../Material";
 import downloadResume from "../../lib/downloadResume";
 // Icons
@@ -13,44 +13,39 @@ interface PreviewControlsProps {
   setShowPDFModal: (show: boolean) => void;
 }
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    marginBottom: theme.spacing(2),
-    "&--action": {
-      marginRight: theme.spacing(2),
-      "&:last-child": {
-        marginRight: 0,
-      },
-    },
-  },
-}));
-
 export const PreviewControls: FunctionComponent<PreviewControlsProps> = ({
   resume,
   goTo,
   setShowPDFModal,
 }) => {
-  const classes = useStyles();
-
   return (
-    <Box display="flex" flexDirection="row" justifyContent="space-between">
+    <Box
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-between"
+      marginBottom={2}
+    >
       {/* Back to overview */}
       <Button
         variant="contained"
         startIcon={<ArrowBackIcon />}
         onClick={() => goTo(`/overview`)}
-        className={classes.button}
       >
         Back to overview
       </Button>
-      <Box marginLeft={2}>
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        flexWrap="wrap"
+        gridGap={8}
+        marginLeft={2}
+      >
         {/* Download as */}
         <DropdownButton
           variant="contained"
           actions={["PDF", "DOCX"]}
           startIcon={<GetAppIcon />}
           onClick={(action) => downloadResume(resume, action)}
-          className={`${classes.button} ${classes.button}--action`}
         >
           Download as..
         </DropdownButton>
@@ -58,7 +53,6 @@ export const PreviewControls: FunctionComponent<PreviewControlsProps> = ({
         <SpacedButton
           variant="contained"
           startIcon={<VisibilityIcon />}
-          className={`${classes.button} ${classes.button}--action`}
           onClick={() => setShowPDFModal(true)}
         >
           Preview
